c874f9b03c91644cce54cd93827478dab5bb6c2b
christian <christiangouw168@gmail.com>
2025-11-14 00:41:41 +0700

    fix: update minHeight calculation in MobileContainer

--- components/layout/MobileContainer.tsx
+++ components/layout/MobileContainer.tsx
@@ -21,7 +21,7 @@ export default function MobileContainer({
 			className={cn("py-3 bg-zinc-50 px-4", noPadding && "p-0", className)}
 			style={{
 				...(withMinHeight && {
-					minHeight: `calc(100vh - ${APP_BAR_MAX_HEIGHT}px)`,
+					minHeight: `calc(100vh - 57px)`,
 				}),
 			}}
 			{...props}
